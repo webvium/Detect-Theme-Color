bc2203cac588f1056629ca7e32d21d4d24378843
Melvin Jones Repol <mrepol742@gmail.com>
2022-03-18 11:38:30 +0800

    Initial Commit

---
+++ inject.js
@@ -0,0 +1,9 @@
+javascript:window.WebviumIdentifierUniqueGeneratedID.theme((function (){
+                                    const metas = document.getElementsByTagName('meta');
+                                    for (let i = 0; i < metas.length; i++) {
+                                        if (metas[i].getAttribute('name') === 'theme-color') {
+                                            return metas[i].getAttribute('content');
+                                        }
+                                    }
+                                    return '';
+                                })());
